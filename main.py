ca0e7df27125329eda50e51c3705650e2c78fc11
kurugit <david.breier@outlook.de>
2025-03-03 02:06:06 +0100

    wordCount Function geschrieben

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ def getBook(path_to_file):
     with open(path_to_file) as file:
         text = file.read()
     return text
+
+def wordCount(text):
+    list = text.split()
+    num = len(list)
+    return num 
+
 def main():
     text = getBook('books/frankenstein.txt')
     print(text)
+    print(f"{wordCount(text)} words found in the document")
 
 if __name__ == "__main__":
     main()
