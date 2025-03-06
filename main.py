129efba3b40b5c72c946204047fe0f30c79d31f7
kurugit <david.breier@outlook.de>
2025-03-06 02:15:07 +0100

    Angefangen final Report zu schreiben

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from stats import wordCount
+from stats import sort_dict
+from collections import Counter
 
 def getBook(path_to_file):
     with open(path_to_file) as file:
@@ -7,8 +9,16 @@ def getBook(path_to_file):
 
 def main():
     text = getBook('books/frankenstein.txt')
-    print(text)
+    text2 = text.lower()
+    count = Counter(text2)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"{wordCount(text)} words found in the document")
+    print("--------- Character Count -------")
+    print(sort_dict(count))
+    print(============= END ===============)
+
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,4 +1,16 @@
 def wordCount(text):
     list = text.split()
     num = len(list)
-    return num 
+    return num
+ 
+def sort_on(dict):
+    return dict["key"]
+
+def sort_dict(dict):
+    alpha_dict = []
+    for key in dict:
+        if key.isalpha():
+            alpha_dict.append(key) 
+    alpha_dict.sort(reverse=True, key=sort_on)
+    return alpha_dict
+    
