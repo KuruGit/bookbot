71a9d2b93cbbfc6b73d2fa3060da2972e776b01b
kurugit <david.breier@outlook.de>
2025-03-07 00:35:35 +0100

    sort_dict neu geschrieben

--- stats.py
+++ stats.py
@@ -3,14 +3,10 @@ def wordCount(text):
     num = len(list)
     return num
  
-def sort_on(dict):
-    return dict["key"]
-
 def sort_dict(dict):
-    alpha_dict = []
     for key in dict:
-        if key.isalpha():
-            alpha_dict.append(key) 
-    alpha_dict.sort(reverse=True, key=sort_on)
-    return alpha_dict
+        if !key.isalpha():
+            del dict[key]
+    dict(sorted(dict.items()))
+    return dict
     
